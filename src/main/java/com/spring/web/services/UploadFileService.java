dd3924eb9feea209c5dbf43c8d5f7d876a8af9f2
StivenCR7 <stivengg234@gmail.com>
2025-02-23 00:23:17 -0500

    Update UploadFileService.java

--- src/main/java/com/spring/web/services/UploadFileService.java
+++ src/main/java/com/spring/web/services/UploadFileService.java
@@ -10,22 +10,29 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service 
 public class UploadFileService {
-	private String folder="images//";
-	
-	public String saveImage(MultipartFile  file) throws IOException {
-		if (!file.isEmpty()) {
-			byte [] bytes=file.getBytes();
-			Path path =Paths.get(folder+file.getOriginalFilename());
-			Files.write(path, bytes);
-			return file.getOriginalFilename();
-		}
-		return "default.jpg";
-	}
-	
-	public void deleteImage(String nombre) {
-		String ruta="images//";
-		File file= new File(ruta+nombre);
-		file.delete();
-	}
-	
+    // Utiliza la variable de entorno HOME para definir la ruta
+    private final String folder = System.getenv("HOME") + "/images/";
+    
+    public String saveImage(MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            // Asegurarse de que el directorio existe
+            Path folderPath = Paths.get(folder);
+            if (!Files.exists(folderPath)) {
+                Files.createDirectories(folderPath);
+            }
+            
+            byte[] bytes = file.getBytes();
+            Path path = folderPath.resolve(file.getOriginalFilename());
+            Files.write(path, bytes);
+            return file.getOriginalFilename();
+        }
+        return "default.jpg";
+    }
+    
+    public void deleteImage(String nombre) {
+        String ruta = System.getenv("HOME") + "/images/";
+        File file = new File(ruta + nombre);
+        file.delete();
+    }
 }
+
