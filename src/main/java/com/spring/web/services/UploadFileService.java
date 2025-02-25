3257494b7f9483b68d22ea462e6f7059f5f2c287
StivenCR7 <stivengg234@gmail.com>
2025-02-24 22:07:58 -0500

    Update UploadFileService.java

--- src/main/java/com/spring/web/services/UploadFileService.java
+++ src/main/java/com/spring/web/services/UploadFileService.java
@@ -1,14 +1,11 @@
 package com.spring.web.services;
 
 import com.azure.storage.blob.*;
-import com.azure.storage.blob.sas.*;
-import com.azure.storage.common.sas.SasProtocol;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Service
@@ -50,14 +47,9 @@ public class UploadFileService {
                 }
 
                 // Opción de prueba: retornar la URL sin SAS token (descomentar para testear)
-                // String blobUrl = blobClient.getBlobUrl();
-                // System.out.println("Blob URL sin SAS: " + blobUrl);
-                // return blobUrl;
-
-                // Generar y retornar la URL con SAS token válido por 2 semanas
-                String sasUrl = generateSasToken(blobClient);
-                System.out.println("Blob URL con SAS token: " + sasUrl);
-                return sasUrl;
+                String blobUrl = blobClient.getBlobUrl();
+                System.out.println("Blob URL sin SAS: " + blobUrl);
+                return blobUrl;
             } catch (Exception ex) {
                 System.err.println("Error en saveImage:");
                 ex.printStackTrace();
@@ -80,25 +72,6 @@ public class UploadFileService {
             }
         } catch (Exception ex) {
             System.err.println("Error en deleteImage:");
-            ex.printStackTrace();
-            throw ex;
-        }
-    }
-
-    private String generateSasToken(BlobClient blobClient) {
-        try {
-            // Definir permisos para el SAS (solo lectura)
-            BlobSasPermission sasPermission = new BlobSasPermission().setReadPermission(true);
-            // Establecer tiempo de expiración a 2 semanas
-            OffsetDateTime expiryTime = OffsetDateTime.now().plusWeeks(2);
-            // Construir el SAS token
-            BlobServiceSasSignatureValues sasValues = new BlobServiceSasSignatureValues(expiryTime, sasPermission)
-                    .setProtocol(SasProtocol.HTTPS_ONLY);
-            String sasToken = blobClient.generateSas(sasValues);
-            System.out.println("SAS token generado: " + sasToken);
-            return blobClient.getBlobUrl() + "?" + sasToken;
-        } catch (Exception ex) {
-            System.err.println("Error en generateSasToken:");
             ex.printStackTrace();
             throw ex;
         }
